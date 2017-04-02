80be08237cff9b4abbb3a4a4f14f3bcc6056eac9
spskauynnda <1033515355@qq.com>
2017-04-02 17:27:41 +0800

    【App】 初步分工

--- app/src/components/app.js
+++ app/src/components/app.js
@@ -3,18 +3,36 @@ import React, {Component} from 'react';
 require('console-polyfill');
 
 import './app.less'
+import topImg from './../images/topImg.png'
 class AppComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        }
+        };
     }
 
     render =()=> {
-
         return (
             <div className="App">
-                hello world
+                <div className="topBar">
+                    <img src={topImg}/>
+                </div>
+                <div className="nav">
+                    <ul>
+                        <li className="nav-item">
+                            <span>东秦与我</span>
+                        </li>
+                        <li className="nav-item">
+                            <span>校庆动态</span>
+                        </li>
+                        <li className="nav-item">
+                            <span>校友与基金</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="main"></div>
+                <div className="more"></div>
+                <div className="footer"></div>
             </div>
         )
     }
