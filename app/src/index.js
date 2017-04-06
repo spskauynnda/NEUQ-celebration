a57efa4017acc34482e29f9c372a3f35121f1f2b
Rachel <15033592279@163.com>
2017-04-06 18:44:00 +0800

    第10002次提交

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {render} from 'react'
 import AppComponent from './components/app';
-
-ReactDOM.render(
-    <AppComponent />,
-    document.getElementById('app')
+import {Router,Route,hashHistory}from 'react-router'
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={AppComponent}>
+        </Route>
+    </Router>), document.getElementById('app')
 );
